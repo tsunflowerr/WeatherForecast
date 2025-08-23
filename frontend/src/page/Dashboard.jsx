a96e07c8dc315b9d19f32d237a492946eb2a12c9
tsunflowerr <23020155@vnu.edu.vn>
2025-08-23 22:37:00 +0700

    loading update

--- frontend/src/page/Dashboard.jsx
+++ frontend/src/page/Dashboard.jsx
@@ -10,6 +10,7 @@ const Dashboard = () => {
     const [error, setError] = useState(null);
     const [suggestions, setSuggestions] = useState([]);
     const [showSuggestions, setShowSuggestions] = useState(false);
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
         const fetchSuggestions = async () => {
@@ -38,6 +39,7 @@ const Dashboard = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         if(searchCity.trim() !== "") {
+            setLoading(true);
             try {
                 const response = await axios.get(`https://weatherforecast-backend-k0c3.onrender.com/api/weather/${searchCity}`);
                 if(response.data) {
@@ -45,26 +47,28 @@ const Dashboard = () => {
                     setError(null);
                 } else {
                     setError("Không tìm thấy thành phố nào phù hợp");
+                    setLoading(false);
                 }
-                
             }
             catch (err) {
                 setError("Đã xảy ra lỗi khi tìm kiếm");
+                setLoading(false);
+            }
         }
-     }
     }
 
     const handleSuggestionClick = (city) => {
         setSearchCity(city);
+        setLoading(true);
         setShowSuggestions(false);
         navigate(`/weather/${city}`);
     };
     return (
     <header className="h-screen w-screen font-sans">
-      <div className="flex items-center justify-between h-40 bg-blue-50">
-        <div className="flex items-center gap-2 cursor-pointer ml-40" onClick={() => navigate("/")}>
-          <Sun className="w-20 h-20 text-amber-500" />
-          <span className="font-extrabold text-2xl bg-gradient-to-r from-gray-900 via-gray-800 to-gray-700 bg-clip-text text-transparent tracking-wide">Weather Forecast</span>
+      <div className="flex items-center justify-between h-40 bg-blue-50 sticky top-0 md:sticky md:top-0">
+        <div className="flex items-center gap-2 cursor-pointer ml-4 md:ml-40" onClick={() => navigate("/")}>
+          <Sun className="w-12 h-12 md:w-20 md:h-20 text-amber-500" />
+          <span className="font-extrabold text-lg md:text-2xl bg-gradient-to-r from-gray-900 via-gray-800 to-gray-700 bg-clip-text text-transparent tracking-wide">Weather Forecast</span>
         </div>
       </div>
 
@@ -78,7 +82,7 @@ const Dashboard = () => {
                 onChange={(e) => setSearchCity(e.target.value)}
                 onFocus={() => setShowSuggestions(true)}
                 placeholder="Nhập tên thành phố...."
-                className="text-gray-700 text-xl font-sans w-128 h-16 border border-gray-700 rounded-3xl px-3 py-2"/>
+                className="text-gray-700 text-base md:text-xl font-sans w-64 md:w-128 h-12 md:h-16 border border-gray-700 rounded-3xl px-3 py-2"/>
                 {showSuggestions && suggestions.length > 0 && (
                 <div className="absolute top-full left-0 w-full bg-white border border-gray-200 rounded-lg shadow-lg z-10 mt-1">
                     {suggestions.map((suggestion, index) => (
@@ -93,10 +97,11 @@ const Dashboard = () => {
                 )}
             </div>
 
-            <button type="submit" className="bg-blue-500 h-16 w-30 text-xl text-white rounded-3xl">Tìm kiếm</button>
+            <button type="submit" className="bg-blue-500 h-12 md:h-16 w-20 md:w-30 text-base md:text-xl cursor-pointer text-white rounded-3xl">Tìm kiếm</button>
          </div>
             {error && <p className="text-red-500 text-center mt-4">{error}</p>}
         </form>
+        {loading && <p className="text-gray-600 text-center font-extrabold font-sans text-3xl mt-20">Đang tải...</p>}
     </header>
     );
 }
--- frontend/src/page/WeatherDetail.jsx
+++ frontend/src/page/WeatherDetail.jsx
@@ -11,8 +11,10 @@ const WeatherDetails = () => {
     const navigate = useNavigate();
     const [weatherData, setWeatherData] = React.useState(null);
     const [error, setError] = React.useState(null);
+    const [loading, setLoading] = React.useState(false);
     useEffect(() => {
         const fetchWeather = async () => {
+        setLoading(true);
         try {
         const response = await axios.get(`https://weatherforecast-backend-k0c3.onrender.com/api/weather/${city}`);
         const data = response.data;
@@ -40,10 +42,12 @@ const WeatherDetails = () => {
             }))
         });
         setError(null);
+        setLoading(false);
         }
         catch (err) {
         setError("Không tìm thấy dữ liệu cho thành phố này");
         setWeatherData(null);
+        setLoading(false);
         }
     };
         if(city) {
@@ -53,46 +57,49 @@ const WeatherDetails = () => {
 
     return (
           <div className="iteam-center justify-between h-screen w-screen font-sans">
-            <div className="sticky top-0 flex items-center justify-between h-40 bg-blue-50">
-              <div className="flex items-center gap-2 cursor-pointer ml-40" onClick={() => navigate("/")}>
-                <Sun className="w-20 h-20 text-amber-500" />
-                <span className="font-extrabold text-2xl bg-gradient-to-r from-gray-900 via-gray-800 to-gray-700 bg-clip-text text-transparent tracking-wide">Weather Forecast</span>
+            <div className="sticky top-0 md:sticky md:top-0 flex items-center justify-between h-40 bg-blue-50">
+              <div className="flex items-center gap-2 cursor-pointer ml-4 md:ml-40" onClick={() => navigate("/")}>
+                <Sun className="w-12 h-12 md:w-20 md:h-20 text-amber-500" />
+                <span className="font-extrabold text-lg md:text-2xl bg-gradient-to-r from-gray-900 via-gray-800 to-gray-700 bg-clip-text text-transparent tracking-wide">Weather Forecast</span>
               </div>
             </div>
+              {loading && (
+                <p className="text-gray-600 text-center font-extrabold font-sans text-3xl mt-20 ">Đang tải...</p>
+              )}
               {error && (
                 <p className="text-red-500 text-center mt-6">{error}</p>
               )}
               {weatherData && (
-                <div className="h-screen bg-gray-50 flex flex-col items-center ">
-                  <div className="bg-white h-80 w-160 border-gray-200 border rounded-4xl mt-10 flex flex-col">
-                    <div className="px-5 py-3">
-                      <span className="text-2xl font-bold text-gray-700 font-sans">Current weather</span> 
+                <div className="h-screen bg-gray-50 flex flex-col items-center">
+                  <div className="bg-white h-auto md:h-80 w-[90%] md:w-160 border-gray-200 border rounded-4xl mt-10 flex flex-col p-4">
+                    <div className="px-2 md:px-5 py-3">
+                      <span className="text-xl md:text-2xl font-bold text-gray-700 font-sans">Current weather</span> 
                     </div>
-                    <div className="flex-grow flex items-center px-12 gap-10">
-                      <img src={weatherData.current.icon} alt={weatherData.current.condition} className="w-32 h-32 ml-5"/>
-                      <div className="font-medium flex-col ml-15 flex justify-center">
-                        <h2 className="text-3xl font-bold text-gray-800">{weatherData.current.city}, {weatherData.current.country}</h2>
-                        <p className="text-xl text-gray-600">{weatherData.current.condition}</p>
-                        <p className="text-5xl font-extrabold text-gray-800">{weatherData.current.temperature}°C</p>
-                        <p className="text-md text-gray-500">Humidity: {weatherData.current.humidity}% | Wind: {weatherData.current.wind_kph} kph</p>   
-                        <p className="text-sm text-gray-400">Last updated: {weatherData.current.last_updated}</p>
+                    <div className="flex-grow flex flex-col md:flex-row items-center px-4 md:px-12 gap-4 md:gap-10">
+                      <img src={weatherData.current.icon} alt={weatherData.current.condition} className="w-24 h-24 md:w-32 md:h-32 md:ml-5"/>
+                      <div className="font-medium flex-col text-center md:text-left md:ml-15 flex justify-center">
+                        <h2 className="text-2xl md:text-3xl font-bold text-gray-800">{weatherData.current.city}, {weatherData.current.country}</h2>
+                        <p className="text-lg md:text-xl text-gray-600">{weatherData.current.condition}</p>
+                        <p className="text-4xl md:text-5xl font-extrabold text-gray-800">{weatherData.current.temperature}°C</p>
+                        <p className="text-sm md:text-md text-gray-500">Humidity: {weatherData.current.humidity}% | Wind: {weatherData.current.wind_kph} kph</p>   
+                        <p className="text-xs md:text-sm text-gray-400">Last updated: {weatherData.current.last_updated}</p>
                       </div>
                     </div>
                   </div>
 
-                  <div className="bg-white h-60 w-360 border-gray-200 border rounded-4xl mt-5 flex flex-col mb-10">
-                    <div className="px-5 py-3">
-                      <span className="text-2xl font-bold text-gray-700 font-sans ">3-Day Forecast</span>
+                  <div className="bg-white h-auto md:h-60 w-[90%] md:w-360 border-gray-200 border rounded-4xl mt-5 flex flex-col mb-10 p-4">
+                    <div className="px-2 md:px-5 py-3">
+                      <span className="text-xl md:text-2xl font-bold text-gray-700 font-sans">3-Day Forecast</span>
                     </div>
-                    <div className="flex-grow flex items-center justify-around gap-10">
+                    <div className="flex-grow flex flex-col md:flex-row items-center justify-around gap-4 md:gap-10">
                       {weatherData.forecast.map((day) => (
-                       <div className="flex flex-row items-center gap-8 w-100 h-40 bg-gray-50 border border-gray-200 rounded-3xl mb-4 p-4" key={day.date}>
-                        <div key={day.date} className="flex flex-col items-center">
-                          <p className="text-md text-gray-600 font-medium">{new Date(day.date).toLocaleDateString()}</p>
-                          <img src={day.icon} alt={day.condition} className="w-20 h-20"/>
-                          <p className="text-lg text-gray-800 font-semibold text-shadow-md ">{day.condition}</p>
+                       <div className="flex flex-row items-center justify-between gap-15 w-full md:w-100 h-auto md:h-40 bg-gray-50 border border-gray-200 rounded-3xl mb-4 p-4" key={day.date}>
+                        <div key={day.date} className="flex flex-col items-center w-45 h-35 justify-center ">
+                          <p className="text-sm md:text-md text-gray-600 font-medium">{new Date(day.date).toLocaleDateString()}</p>
+                          <img src={day.icon} alt={day.condition} className="w-16 h-16 md:w-20 md:h-20"/>
+                          <p className="text-base md:text-lg text-gray-800 font-semibold text-shadow-md">{day.condition}</p>
                         </div>
-                        <div className="text-left gap 2 flex flex-col">
+                        <div className="text-left gap-0.5 flex flex-col text-sm md:text-md mr-10">
                           <p className="font-semibold text-gray-700">Max: {day.maxtemp_c}°C</p>
                           <p className="font-semibold text-gray-700">Min: {day.mintemp_c}°C</p>
                           <p className="font-semibold text-gray-700">Avg: {day.avgtemp_c}°C</p>
